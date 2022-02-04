f8d81e22d12e4de8f95eb0bc15628748334b987a
Nicola Rubini <nicola.rubini@cern.ch>
2022-02-04 14:41:29 +0100

    Compile Macro fix 2

--- Run.sh
+++ Run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
-    g++ ./src/Utilities/MCG_PhiAnalysis.C \
+    g++ ./MCG_PhiAnalysis.C \
     -o ./exe/MCG_PhiAnalysis \
     -std=c++11 \
-    `/Users/nikolajal/alice/sw/osx_x86-64/ROOT/latest/bin/root-config --cflags --ldflags --libs --glibs --evelibs` \
-    `/Applications/pythia8306/bin/pythia8-config --cflags --libs`
+    `root-config --cflags --ldflags --libs --glibs --evelibs` \
+    `pythia8-config --cflags --libs`
 
