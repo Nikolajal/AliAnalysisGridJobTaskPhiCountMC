75929e09e3c2d9f15291f9c52a6d255f45537519
Nicola Rubini <nicola.rubini@cern.ch>
2022-02-04 14:42:30 +0100

    Compile Macro fix 3

--- Run.sh
+++ Run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
     g++ ./MCG_PhiAnalysis.C \
-    -o ./exe/MCG_PhiAnalysis \
+    -o ./MCG_PhiAnalysis \
     -std=c++11 \
     `root-config --cflags --ldflags --libs --glibs --evelibs` \
     `pythia8-config --cflags --libs`
