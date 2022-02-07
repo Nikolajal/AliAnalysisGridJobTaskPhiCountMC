eed28e01b04596f301651e3d53157f9c572df9ac
Nicola Rubini <nicola.rubini@cern.ch>
2022-02-07 08:54:30 +0100

    New run setup

--- Run.sh
+++ Run.sh
@@ -1,8 +1,34 @@
 #!/usr/bin/env bash
 #
-    g++ ./MCG_PhiAnalysis.C \
-    -o ./MCG_PhiAnalysis \
-    -std=c++11 \
-    `root-config --cflags --ldflags --libs --glibs --evelibs` \
-    `pythia8-config --cflags --libs`
+g++ ./MCG_PhiAnalysis.C -o ./MCG_PhiAnalysis -std=c++11 `root-config --cflags --ldflags --libs --glibs --evelibs` `pythia8-config --cflags --libs`
+#
+mkdir -p result/MC_Production/
 
+strun=0
+nruns=$((10-$strun))
+njobs=10
+nevents=100
+
+for run in $(seq $strun $(($strun + $nruns - 1))); do
+
+    ### wait if there are too many jobs running
+    while true; do
+        bkgjobs=$(ps aux | grep MCG_PhiAnalysis | wc -l | xargs)
+        if [ $bkgjobs -lt $(($njobs +1)) ]; then
+            break
+        fi
+        echo "[---] sleep while waiting for a free job slot"
+        sleep 60
+    done
+    
+    runid=$(printf "%05d" $run)
+    seed=$((123456789 + $run * 2))
+    
+    echo "[---] starting run: $runid"
+
+    ./exe/MCG_PhiAnalysis result/MC_Production/outGeneratorMC_$runid.root $nevents $runid 0 >& ./result/MC_Production/log.$runid.log &
+
+    sleep 1s
+
+done
+exit 0
