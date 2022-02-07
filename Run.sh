2c96efcf5b48d88d471ce65aee5a645bd2d921ec
Nicola Rubini <nicola.rubini@cern.ch>
2022-02-07 18:16:55 +0100

    new add

--- Run.sh
+++ Run.sh
@@ -4,32 +4,5 @@ g++ ./MCG_PhiAnalysis.C -o ./MCG_PhiAnalysis -std=c++11 `root-config --cflags --
 #
 mkdir -p result/Data/
 mkdir -p result/Logs/
-
-strun=0
-nruns=$((10-$strun))
-njobs=10
-nevents=100
-
-for run in $(seq $strun $(($strun + $nruns - 1))); do
-
-    ### wait if there are too many jobs running
-    while true; do
-        bkgjobs=$(ps aux | grep MCG_PhiAnalysis | wc -l | xargs)
-        if [ $bkgjobs -lt $(($njobs +1)) ]; then
-            break
-        fi
-        echo "[---] sleep while waiting for a free job slot"
-        sleep 60
-    done
-    
-    runid=$(printf "%05d" $run)
-    seed=$((123456789 + $run * 2))
-    
-    echo "[---] starting run: $runid"
-
-    ./MCG_PhiAnalysis ./result/Data/outGeneratorMC_$runid $nevents $runid 0 >& ./result/Logs/log.$runid.log &
-
-    sleep 1s
-
-done
-exit 0
+/MCG_PhiAnalysis ./result/Data/Pythia8_$1_$2_$3_$4 $1 $2 $3 $4 0 >& ./result/Logs/log.$runid.log &
+#
