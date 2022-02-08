6c011a5c4de41afe79b7cf1f7ffeaa60b0a17590
Nicola Rubini <nicola.rubini@cern.ch>
2022-02-08 10:18:43 +0100

    No work on grif test

--- Run.sh
+++ Run.sh
@@ -2,7 +2,7 @@
 #
 g++ ./MCG_PhiAnalysis.C -o ./MCG_PhiAnalysis -std=c++11 `root-config --cflags --ldflags --libs --glibs --evelibs` `pythia8-config --cflags --libs`
 #
-mkdir -p result/Data/
-mkdir -p result/Logs/
-/MCG_PhiAnalysis ./result/Data/Pythia8_$1_$2_$3_$4 $1 $2 $3 $4 0 >& ./result/Logs/log.$runid.log &
+touch result.root
+#./MCG_PhiAnalysis Pythia8_$1_$2_$3_$4 $1 $2 $3 $4 >& ./Pythia8_$1_$2_$3_$4.log &
 #
+ 
